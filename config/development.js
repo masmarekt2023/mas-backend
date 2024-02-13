a0833220401d62e2782a9977b8444f06203d687b
masmarekt2023 <127682794+masmarekt2023@users.noreply.github.com>
2024-02-13 09:43:46 +0300

    Update development.js

--- config/development.js
+++ config/development.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 module.exports = {
   port: 1865,
-  dpUrl: `mongodb+srv://mo61920:WMQHSKk4kVlJbvTF@mas2023.qshesn6.mongodb.net/dapps-data?retryWrites=true&w=majority`,
+  dpUrl: `mongodb+srv://mo61920:7bQmwaCoOJCDLjqc@mas2023.qshesn6.mongodb.net/dapps-data?retryWrites=true&w=majority`,
   jwtsecret: "CVD!S9Fm7$M#8a",
   /*nodemailer: {
     service: "Zoho",
